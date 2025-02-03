59e1965130bc4e48ef21576e3f54c7305c51f367
reuben longley <reuben.longley@clarionhg.com>
2025-02-03 14:17:05 +0000

    readinglist

--- models/ReadingList.js
+++ models/ReadingList.js
@@ -1,18 +1,35 @@
 class ReadingList {
-    constructor(id, title, author, genre, summary, book_type, isRead = 'N') {
-        this.id = id
-        this.title = title
-        this.author = author
-        this.genre = genre
-        this.summary = summary
-        this.book_type = book_type
-        this.isRead = isRead === 'Y'
+    constructor( isRead = 'N') 
     }
 
     markAsRead() {
         this.isRead = true
     }
+
+
+
+static async addBooktolist(book_id, user_id) {
+    const query = 'INSERT INTO ReadingList WHERE (book_id AND user_id) VALUES (?, ?)'   
+    await db.raw(query, [book_id, user_id]);
+    return results [0]
 }
+
+static async removefromlist(book_id, user_id) {
+    const query = 'DELETE FROM ReadingList WHERE (book_id AND user_id) VALUES (?, ?)'   
+    await db.raw(query, [book_id, user_id]);
+    return results [0]
+}
+
+static async editreadstatus (book_id, user_id, read_status) {
+    const query = 'UPDATE ReadingList SET (book_id AND user_id AND read_status) VALUES (?, ?, ?)'   
+    await db.raw(query, [book_id, user_id, read_status]);
+    return results [0]
+
+
+
+
+
 
 export default ReadingList
 //test
+
