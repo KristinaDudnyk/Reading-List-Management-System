cb04156d2d8a96c47ed51ad3546c15c0b216de75
KristinaDudnyk <kristina.dudnyk20@gmail.com>
2025-02-03 12:52:41 +0000

    Created API for Users + knex setup

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import User from "../models/Userjs";
 
 const app = express();
 app.set("views", "views");
@@ -9,48 +10,46 @@ app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
+// GET user
+app.get("/auth/login", async (req, res) => {
+  const { username, email } = req.body;
+  const user = await User.findUser(username, email);
+  res.render("readingList.ejs", { user });
+});
 
-// Query User database 
-app.get("/", async (req, res) => {
+// POST user
+app.post("/auth/register", async (req, res) => {
+  const { username, first_name, last_name, email } = req.body;
+  const user = await User.createUser(username, first_name, last_name, email);
+  res.render("readingList.ejs", { user });
+});
 
-})
+// DELETE user
+app.post("/auth/register", async (req, res) => {
+  const { username, email } = req.body;
+  const user = await User.deleteUser(username, email);
+  res.render("books.ejs", { user });
+});
 
 //Query book details -  return all details
 
-app.get("/", async (req, res) => {
-
-})
-
+app.get("/", async (req, res) => {});
 
 //Create book details
 
-app.post("/", async (req, res) => {
-
-})
-
+app.post("/", async (req, res) => {});
 
 //Editing book details
-app.post("/", async (req, res) => {
+app.post("/", async (req, res) => {});
 
-})
-
-
-//Adding book to reading list including error handling 
-app.post("/", async (req, res) => {
-
-})
+//Adding book to reading list including error handling
+app.post("/", async (req, res) => {});
 
 //Delete book from reading list
-app.delete("/", async (req, res) => {
-
-})
+app.delete("/", async (req, res) => {});
 
 //Edit reading list of read status
-app.post("/", async (req, res) => {
-
-})
+app.post("/", async (req, res) => {});
 
 //Query user statistics
-app.get("/", async (req, res) => {
-
-})
+app.get("/", async (req, res) => {});
