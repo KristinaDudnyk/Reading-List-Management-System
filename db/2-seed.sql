0b7a4c4937d7af64cd50548038cc3035f244d555
KristinaDudnyk <kristina.dudnyk20@gmail.com>
2025-02-05 16:46:27 +0000

    added books covers

--- db/2-seed.sql
+++ db/2-seed.sql
@@ -20,7 +20,12 @@ VALUES
     'Bergkamp',
     'Dennis.Bergkamp@gmail.com'
   ),
-  ('Beth_Mead', 'Beth', 'Mead', 'Beth.Mead@gmail.com'),
+  (
+    'Beth_Mead',
+    'Beth',
+    'Mead',
+    'Beth.Mead@gmail.com'
+  ),
   (
     'Leah_Williamson',
     'Leah',
@@ -30,7 +35,7 @@ VALUES
 
 --books--
 INSERT INTO
-  books (title, author, genre, summary, book_type)
+  books (title, author, genre, summary, book_type, cover)
 VALUES
   (
     'Dogger',
@@ -38,6 +43,7 @@ VALUES
     'kids',
     'When Dave loses his favourite toy, Dogger, he is desolate. But then Dogger turns up at the school summer fair, and everything seems all right - until someone else buys him before Dave can get the money!',
     'paperback',
+    'https://m.media-amazon.com/images/I/61uwRgU2qTL._AC_UF894,1000_QL80_.jpg'
   ),
   (
     'Emma',
@@ -45,6 +51,7 @@ VALUES
     'Fiction',
     'Emma Woodhouse is one of Austens most captivating and vivid characters. Beautiful, spoilt, vain and irrepressibly witty, Emma organizes the lives of the inhabitants of her sleepy little village and plays matchmaker with devastating effect.',
     'hardback',
+    'https://rochizalani.com/wp-content/uploads/2020/12/Emma.jpg'
   ),
   (
     'Return Of The Dambusters',
@@ -52,6 +59,7 @@ VALUES
     'Non-fiction',
     'Former RAF Tornado Navigator and Gulf War veteran John Nichol sets out on a personal journey to discover what happened to 617 Squadron after the flood. The role RAF 617 Squadron in the destruction of the dams at the heart of the industrial Ruhr has been celebrated in book, magazine and film for more than seventy years.',
     'hardback',
+    'https://m.media-amazon.com/images/I/81SpN+MZPQL._AC_UF1000,1000_QL80_.jpg'
   ),
   (
     'The Mermaid',
@@ -59,6 +67,7 @@ VALUES
     'Fiction',
     'This book tells the enchanting tale of Amelia, a mermaid who leaves her ocean home for love. She marries a fisherman named Jack, but after he tragically disappears, she finds herself at P.T. Barnums American Museum as the real Fiji mermaid. Here, she discovers that escaping may be more challenging than she anticipated.',
     'e-book',
+    'https://m.media-amazon.com/images/I/81IOrLxmt2L._AC_UF894,1000_QL80_.jpg'
   ),
   (
     'Dr Jekyll And Mr Hyde',
@@ -66,6 +75,46 @@ VALUES
     'Fiction',
     'Published as a shilling shocker, Robert Louis Stevensons dark psychological fantasy gave birth to the idea of the split personality. The story of respectable Dr Jekylls strange association with damnable young man Edward Hyde; the hunt through fog-bound London for a killer; and the final revelation of Hydes true identity is a chilling exploration of humanitys basest capacity for evil.',
     'paperback',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/JekyllAndHyde1931Poster.jpg/800px-JekyllAndHyde1931Poster.jpg'
+  ) (
+    'The Silent Symphony',
+    'Eleanor Vance',
+    'Mystery',
+    'A renowned composer is found dead in his soundproof studio, leaving behind a cryptic musical score as the only clue. Detective Inspector Anya Sharma must decipher the melody of silence to catch a killer.',
+    'Fiction',
+    'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1688288114l/182525608.jpg'
+  ),
+  (
+    'Echoes of the Nebula',
+    'Jax Orion',
+    'Science Fiction',
+    'In a distant future where interstellar travel is commonplace, a lone space explorer stumbles upon an ancient alien artifact that holds the key to understanding the origins of the universe. But powerful forces are also searching for the artifact, and they will stop at nothing to obtain it.',
+    'Fiction',
+    'https://m.media-amazon.com/images/I/71qEy9OJ-uL.jpg'
+  ),
+  (
+    'The Whispering Pines',
+    'Sarah Croft',
+    'Thriller',
+    'A young woman inherits her grandmother''s secluded cabin in the woods, only to discover a dark secret hidden within the whispering pines. As she delves deeper into her family''s past, she becomes the target of a ruthless killer who wants to keep the truth buried.',
+    'Fiction',
+    'https://images-eu.ssl-images-amazon.com/images/I/81tvHwuN8oL._AC_UL600_SR600,600_.jpg'
+  ),
+  (
+    'The Alchemist''s Legacy',
+    'Liam Blackwood',
+    'Fantasy',
+    'In a world where magic is both revered and feared, a young apprentice discovers he is the descendant of a legendary alchemist. He must master his newfound powers and unravel the secrets of his lineage to prevent a cataclysmic event that threatens to engulf the realm.',
+    'Fiction',
+    'https://cdn.kobo.com/book-images/19bc5ab3-72f9-41b1-a903-cbbbb72f4977/353/569/90/False/the-alchemist-s-legacy-1.jpg'
+  ),
+  (
+    'The Culinary Chronicles',
+    'Isabella Rossi',
+    'Cookbook',
+    'A collection of delectable recipes from around the world, accompanied by heartwarming stories and culinary tips from renowned chef Isabella Rossi. From classic comfort food to exotic delicacies, this cookbook is a celebration of the art of cooking and the joy of sharing a meal.',
+    'Non-Fiction',
+    'https://m.media-amazon.com/images/I/41mpuYTkTAL._AC_UF894,1000_QL80_.jpg'
   );
 
 I
