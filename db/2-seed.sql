ae4dfffe4c8fb116fb2ff1d2619a8e5806a837c6
KristinaDudnyk <kristina.dudnyk20@gmail.com>
2025-02-05 16:38:12 +0000

    delete column isRead from books table

--- db/2-seed.sql
+++ db/2-seed.sql
@@ -1,22 +1,71 @@
 --users--
-
-INSERT INTO users (username, first_name, last_name, email) VALUES 
-('Reuben_Longley','Reuben','Longley','reuben.longley@gmail.com'),
-('Thierry_Henry','Thierry','Henry','Thierry.Henry@gmail.com'),
-('Dennis_Bergkamp','Dennis','Bergkamp','Dennis.Bergkamp@gmail.com'),
-('Beth_Mead','Beth','Mead','Beth.Mead@gmail.com'),
-('Leah_Williamson','Leah','Williamson','Leah.Williamson@gmail.com');
+INSERT INTO
+  users (username, first_name, last_name, email)
+VALUES
+  (
+    'Reuben_Longley',
+    'Reuben',
+    'Longley',
+    'reuben.longley@gmail.com'
+  ),
+  (
+    'Thierry_Henry',
+    'Thierry',
+    'Henry',
+    'Thierry.Henry@gmail.com'
+  ),
+  (
+    'Dennis_Bergkamp',
+    'Dennis',
+    'Bergkamp',
+    'Dennis.Bergkamp@gmail.com'
+  ),
+  ('Beth_Mead', 'Beth', 'Mead', 'Beth.Mead@gmail.com'),
+  (
+    'Leah_Williamson',
+    'Leah',
+    'Williamson',
+    'Leah.Williamson@gmail.com'
+  );
 
 --books--
-
-INSERT INTO books (title, author, genre, summary, book_type, isRead) VALUES 
-('Dogger', 'Shirley Hughes','kids', 'When Dave loses his favourite toy, Dogger, he is desolate. But then Dogger turns up at the school summer fair, and everything seems all right - until someone else buys him before Dave can get the money!', 'paperback',),
-('Emma', 'Jane Austen','Fiction', 'Emma Woodhouse is one of Austens most captivating and vivid characters. Beautiful, spoilt, vain and irrepressibly witty, Emma organizes the lives of the inhabitants of her sleepy little village and plays matchmaker with devastating effect.', 'hardback',),
-('Return Of The Dambusters', 'John Nichol','Non-fiction','Former RAF Tornado Navigator and Gulf War veteran John Nichol sets out on a personal journey to discover what happened to 617 Squadron after the flood. The role RAF 617 Squadron in the destruction of the dams at the heart of the industrial Ruhr has been celebrated in book, magazine and film for more than seventy years.', 'hardback',),
-('The Mermaid', 'Christina Henry','Fiction', 'This book tells the enchanting tale of Amelia, a mermaid who leaves her ocean home for love. She marries a fisherman named Jack, but after he tragically disappears, she finds herself at P.T. Barnums American Museum as the real Fiji mermaid. Here, she discovers that escaping may be more challenging than she anticipated.', 'e-book',),
-('Dr Jekyll And Mr Hyde', 'Robert Louis Stevenson','Fiction', 'Published as a shilling shocker, Robert Louis Stevensons dark psychological fantasy gave birth to the idea of the split personality. The story of respectable Dr Jekylls strange association with damnable young man Edward Hyde; the hunt through fog-bound London for a killer; and the final revelation of Hydes true identity is a chilling exploration of humanitys basest capacity for evil.', 'paperback',);
-
-
+INSERT INTO
+  books (title, author, genre, summary, book_type)
+VALUES
+  (
+    'Dogger',
+    'Shirley Hughes',
+    'kids',
+    'When Dave loses his favourite toy, Dogger, he is desolate. But then Dogger turns up at the school summer fair, and everything seems all right - until someone else buys him before Dave can get the money!',
+    'paperback',
+  ),
+  (
+    'Emma',
+    'Jane Austen',
+    'Fiction',
+    'Emma Woodhouse is one of Austens most captivating and vivid characters. Beautiful, spoilt, vain and irrepressibly witty, Emma organizes the lives of the inhabitants of her sleepy little village and plays matchmaker with devastating effect.',
+    'hardback',
+  ),
+  (
+    'Return Of The Dambusters',
+    'John Nichol',
+    'Non-fiction',
+    'Former RAF Tornado Navigator and Gulf War veteran John Nichol sets out on a personal journey to discover what happened to 617 Squadron after the flood. The role RAF 617 Squadron in the destruction of the dams at the heart of the industrial Ruhr has been celebrated in book, magazine and film for more than seventy years.',
+    'hardback',
+  ),
+  (
+    'The Mermaid',
+    'Christina Henry',
+    'Fiction',
+    'This book tells the enchanting tale of Amelia, a mermaid who leaves her ocean home for love. She marries a fisherman named Jack, but after he tragically disappears, she finds herself at P.T. Barnums American Museum as the real Fiji mermaid. Here, she discovers that escaping may be more challenging than she anticipated.',
+    'e-book',
+  ),
+  (
+    'Dr Jekyll And Mr Hyde',
+    'Robert Louis Stevenson',
+    'Fiction',
+    'Published as a shilling shocker, Robert Louis Stevensons dark psychological fantasy gave birth to the idea of the split personality. The story of respectable Dr Jekylls strange association with damnable young man Edward Hyde; the hunt through fog-bound London for a killer; and the final revelation of Hydes true identity is a chilling exploration of humanitys basest capacity for evil.',
+    'paperback',
+  );
 
 I
-
